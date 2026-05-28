beaef223c05cdd23e776297357bf6724eb1b4f20
Jumin Cho <77545063+jumincho@users.noreply.github.com>
2026-05-28 14:41:19 +0900

    Add MVVM View, Services and Theme sources

---
+++ Review_Reminder/MVVM/View/ReviewSystemView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace Review_Reminder.MVVM.View
+{
+    public partial class ReviewSystemView : UserControl
+    {
+        public ReviewSystemView()
+        {
+            InitializeComponent();
+        }
+    }
+}
